b904610d9e02081a6c0a7e5a5347d2c3dc0d376c
Ankitgif <ankitsinha2303@gmail.com>
2020-04-26 15:04:05 +0530

    [Ankit] Added TC-1.1 - Should return sad

--- src/test/java/com/moodtesting/MoodAnalyzerTest.java
+++ src/test/java/com/moodtesting/MoodAnalyzerTest.java
@@ -10,6 +10,18 @@ public class MoodAnalyzerTest {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
         String mood = moodAnalyzer.analyseMood("This is a sad message");
         Assert.assertEquals("SAD", mood);
+    }
+    @Test
+    public void givenMessage_WhenHappy_ShouldReturnHappy(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyseMood("This is a happy message");
+        Assert.assertEquals("HAPPY", mood);
 
+    }
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnSad(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyseMood("I am in sad mood");
+        Assert.assertEquals("SAD", mood);
     }
 }
