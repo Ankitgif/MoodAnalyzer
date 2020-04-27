10efa0535ab125053778f5807a1ce3d80344a193
Ankitgif <ankitsinha2303@gmail.com>
2020-04-28 01:52:53 +0530

    [Ankit] Added UC3->TC-3.2 Throw Custom exception for empty mood

--- src/main/java/com/moodtesting/MoodAnalyzer.java
+++ src/main/java/com/moodtesting/MoodAnalyzer.java
@@ -13,13 +13,15 @@ public class MoodAnalyzer {
     }
     public String analyseMood() throws MoodAnalysisException {                           //UC-2 Handle exception
         try {
+            if(message.length() == 0)                                                   //Checking message is empty or not
+                throw new MoodAnalysisException("Mood should not be empty", MoodAnalysisException.exceptionType.EMPTY_MOOD);
             if (message.contains("sad"))
                 return "SAD";
             else
                 return "HAPPY";
         }
-        catch(NullPointerException e){
-            throw new MoodAnalysisException("Please enter proper mood");                //UC-3 Throwing Custom Exception
+        catch(NullPointerException exception){
+            throw new MoodAnalysisException("Please enter proper mood", MoodAnalysisException.exceptionType.NULL_MOOD);                //UC-3 Throwing Custom Exception
             }
     }
 
