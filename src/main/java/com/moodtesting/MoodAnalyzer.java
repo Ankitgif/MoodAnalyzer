73aa2c7613b01be1b073f2e481daafd4f2d0a326
Ankitgif <ankitsinha2303@gmail.com>
2020-04-28 19:55:31 +0530

    [Ankit] Added override equals method

--- src/main/java/com/moodtesting/MoodAnalyzer.java
+++ src/main/java/com/moodtesting/MoodAnalyzer.java
@@ -1,5 +1,7 @@
 package com.moodtesting;
 
+import java.util.Objects;
+
 public class MoodAnalyzer {
 
     private  String message;
@@ -11,7 +13,7 @@ public class MoodAnalyzer {
     public MoodAnalyzer(String message){
         this.message = message;
     }
-    public String analyseMood() throws MoodAnalysisException {                           //UC-2 Handle exception
+    public String analyseMood(String message) throws MoodAnalysisException {                           //UC-2 Handle exception
         try {
             if(message.length() == 0)                                                   //Checking message is empty or not
                 throw new MoodAnalysisException("Mood should not be empty", MoodAnalysisException.exceptionType.EMPTY_MOOD);
@@ -24,6 +26,20 @@ public class MoodAnalyzer {
             throw new MoodAnalysisException("Please enter proper mood", MoodAnalysisException.exceptionType.NULL_MOOD);                //UC-3 Throwing Custom Exception
             }
     }
+    public String analyseMood() throws MoodAnalysisException {
+        return analyseMood(this.message);
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MoodAnalyzer that = (MoodAnalyzer) o;
+        return Objects.equals(message, that.message);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(message);
+    }
 }
