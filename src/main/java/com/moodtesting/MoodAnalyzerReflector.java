a6ab2ff9d9fef2237b2f4f5d16d62894b257b247
Ankitgif <ankitsinha2303@gmail.com>
2020-04-29 23:22:57 +0530

    [Ankit] Added UC7, setFieldValue method

--- src/main/java/com/moodtesting/MoodAnalyzerReflector.java
+++ src/main/java/com/moodtesting/MoodAnalyzerReflector.java
@@ -1,6 +1,7 @@
 package com.moodtesting;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class MoodAnalyzerReflector {
@@ -63,5 +64,17 @@ public class MoodAnalyzerReflector {
             throw new MoodAnalysisException("No Such Method Error",MoodAnalysisException.exceptionType.NO_SUCH_METHOD_ERROR);
         }
         return moodAnalyserObject;
+    }
+    public static void setFieldValue(Object object, String fieldName, String fieldValue){
+        Field field = null;
+        try {
+            field = object.getClass().getField(fieldName);
+            field.setAccessible(true);
+            field.set(object,fieldValue);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
