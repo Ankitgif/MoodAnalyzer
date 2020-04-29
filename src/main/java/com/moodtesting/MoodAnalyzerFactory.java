e1bf217f9eeae131f29162930be6eae1a49c4e57
Ankitgif <ankitsinha2303@gmail.com>
2020-04-29 13:46:57 +0530

    [Ankit] Added UC-5, parameterized constructor

--- src/main/java/com/moodtesting/MoodAnalyzerFactory.java
+++ src/main/java/com/moodtesting/MoodAnalyzerFactory.java
@@ -4,16 +4,41 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class MoodAnalyzerFactory {
-    public static MoodAnalyzer createMoodAnalyzer() throws MoodAnalysisException {
+
+    public static MoodAnalyzer createMoodAnalyzer(String name) throws MoodAnalysisException {
         try{
-            Class<?> moodAnalyzerClass = Class.forName("com.moodtesting.MoodAnaly");
+            Class<?> moodAnalyzerClass = Class.forName(name);
             Constructor<?> moodConstructor = moodAnalyzerClass.getConstructor();
             Object moodObj = moodConstructor.newInstance();
             return (MoodAnalyzer) moodObj;
         } catch (ClassNotFoundException exception) {
             throw new MoodAnalysisException("Please enter proper class name", MoodAnalysisException.exceptionType.CLASS_NOT_FOUND);
         } catch (NoSuchMethodException e) {
+            throw new MoodAnalysisException("No such method error",MoodAnalysisException.exceptionType.NO_SUCH_METHOD_ERROR);
+        } catch (SecurityException e) {
             e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static MoodAnalyzer createMoodAnalyzer(String name, String message) throws MoodAnalysisException {
+        try{
+            Class<?> moodAnalyzerClass = Class.forName(name);
+            Constructor<?> moodConstructor = moodAnalyzerClass.getConstructor(String.class);
+            Object moodObj = moodConstructor.newInstance(message);
+            return (MoodAnalyzer) moodObj;
+        } catch (ClassNotFoundException exception) {
+            throw new MoodAnalysisException("Please enter proper class name", MoodAnalysisException.exceptionType.CLASS_NOT_FOUND);
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalysisException("No such method error",MoodAnalysisException.exceptionType.NO_SUCH_METHOD_ERROR);
         } catch (SecurityException e) {
             e.printStackTrace();
         } catch (InstantiationException e) {
